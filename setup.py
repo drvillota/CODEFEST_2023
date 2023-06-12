c7ee656205eb6a1222d5b8379cb695c482697dac
Miguel Ángel <ma.acostaw@uniandes.edu.co>
2023-06-11 22:58:37 -0500

    otras cosas

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.13'
-DESCRIPTION = 'Streaming video data via networks'
-LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+VERSION = '0.0.1'
+DESCRIPTION = 'Una herramienta de detección de elementos peculiares'
+LONG_DESCRIPTION = 'Falta'
 
 # Setting up
 setup(
     name="co2ncientes",
     version=VERSION,
     author="Co2ncientes (CODEFEST 2023)",
-    author_email="<ma.acostaw@uniandes.com>",
+    author_email="ma.acostaw@uniandes.com",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
@@ -30,7 +30,7 @@ setup(
         "tensorflow",
         "tensorflow_intel",
     ],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    keywords=['python', 'CODEFEST'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
